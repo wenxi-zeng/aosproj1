3f5654ef41cfa89ecfe3e6cbde3033f8f1b830a8
song4u <song4you@msn.cn>
2020-02-15 16:39:03 -0600

    add file helper

---
+++ src/main/java/util/FileHelper.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHelper {
+
+    public static List<String> read(String filename) throws IOException {
+        File file = new File(filename);
+        List<String> lines = new ArrayList<>();
+
+        if (file.exists()) {
+            BufferedReader fileReader = new BufferedReader(new FileReader(filename));
+
+            try {
+                String line = fileReader.readLine();
+
+                while (line != null) {
+                    lines.add(line);
+                    line = fileReader.readLine();
+                }
+            } finally {
+                fileReader.close();
+            }
+        }
+
+        return lines;
+    }
+
+    public static void append(String filename, String line) throws IOException {
+        FileWriter fileWriter = new FileWriter(filename, true);
+        fileWriter.write(line);
+        fileWriter.close();
+    }
+
+    public static boolean exist(String filename) {
+        File file = new File(filename);
+        return file.exists();
+    }
+}
